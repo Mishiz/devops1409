82f6b97e012786fa6f9130824dab6f49865ac849
Anatoly <workmishiz@gmail.com>
2022-03-09 22:49:20 +0300

    Add ProdServer

--- devops1409.tf
+++ devops1409.tf
@@ -72,16 +72,16 @@ resource "digitalocean_droplet" "prod1408" {
   ]
 
   provisioner "file" {
-    source = "/root/.s3cfg"
+    source      = "/root/.s3cfg"
     destination = "/root/.s3cfg"
-    }
+  }
   connection {
     host        = self.ipv4_address
     user        = "root"
     type        = "ssh"
     private_key = file(var.pvt_key)
     timeout     = "2m"
-    }
+  }
 
   provisioner "remote-exec" {
     inline = [
@@ -93,10 +93,7 @@ resource "digitalocean_droplet" "prod1408" {
       "cd /var/lib/tomcat9/webapps"
       "s3cmd get s3://dev1408s/ROOT.war"
     ]
-
-
-
-
+  }
 }
 
 
